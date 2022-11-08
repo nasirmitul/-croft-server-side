061a0ef982a61dc742899bee17c8580383402888
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-08 23:18:36 +0600

    loaded single service data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -20,12 +20,28 @@ async function run() {
     try {
         const serviceCollection = client.db('croft').collection('services');
 
-        //creating api for services
+        //creating api for services for home
         app.get('/home/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
+        //creating api for services for service page
+        app.get('/services', async(req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        //creating api for single service
+        app.get('/service/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
     }
     finally {
