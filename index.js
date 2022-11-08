12742f036cb17240b8695286fb2a274d76f68ce8
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-08 19:41:32 +0600

    loaded services data for home

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 
 //middleware
 app.use(cors())
 app.use(express())
 
 
+//database connection
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.4w35qx6.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('croft').collection('services');
+
+        //creating api for services
+        app.get('/home/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
     res.send("Croft server is running")
 })
 
+
+//run server
 app.listen(port, () => {
     console.log('server is running on port number', port);
 })
